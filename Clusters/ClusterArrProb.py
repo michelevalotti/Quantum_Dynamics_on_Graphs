f688967e97a1e1229d55778037c17bb903b1cfdd
michelevalotti <michele.valotti@gmail.com>
2019-03-08 17:30:29 +0000

    moved functions and added average arrival probability

--- Clusters/ClusterArrProb.py
+++ Clusters/ClusterArrProb.py
@@ -35,30 +35,49 @@ def ClusterGraph(Clusters,ClusterNodes,ClusterConnections):
     return G, pos, TotNodes
 
 
+def ConnectRand(G, ConnTot, Clusters):
 
-def ClusterArrivalProb(G,ConnTrials,steps,InterClusterConn,gamma,eta):
+    TotNodes = G.number_of_nodes()
+    ClusterNodes = G.number_of_nodes()/Clusters
+
+    for InterConn in range(ConnTot):
+        InterNode1 = np.random.randint(0, TotNodes)
+        InterNode2 = np.random.randint(0, TotNodes)
+        while(int(InterNode1/ClusterNodes) == int(InterNode2/ClusterNodes)):
+            InterNode2 = np.random.randint(0, TotNodes)
+
+        G.add_edge(InterNode1,InterNode2)
+    
+    return G
+
+def ConnectNext(G, NextConns, Clusters):
+
+    TotNodes = G.number_of_nodes()
+    ClusterNodes = TotNodes/Clusters
+
+    for CNext in range(NextConns):
+        NextNode1 = np.random.randint(0, TotNodes-(ClusterNodes))
+        NextNode2 = np.random.randint((int(NextNode1/ClusterNodes)+1)*ClusterNodes, ((int(NextNode1/ClusterNodes)+2)*ClusterNodes))
+
+        G.add_edge(NextNode1,NextNode2)
+
+    return G
+
+
+def ClusterArrivalProb(G,ConnTrials,steps,Clusters,InterClusterConn,gamma,eta):
     '''add edges between clusters and record probability that quantum walker reacher cluster on the opposite side'''
 
+    TotNodes = G.number_of_nodes()
     ArrivalProbs = []
     TotInterConn = []
 
-    for InterConn in range(InterClusterConn):
-        InterNode1 = np.random.randint(0, TotNodes)
-        InterNode2 = np.random.randint(0, TotNodes)
-        while(int(InterNode1/ClusterNodes) == int(InterNode2/ClusterNodes)):
-            InterNode2 = np.random.randint(0, TotNodes)
-        
-        G.add_edge(InterNode1,InterNode2)
+    # G = ConnectRand(G, InterClusterConn, Clusters)
+    G = ConnectNext(G, InterClusterConn, Clusters)
 
     for j in tqdm(range(ConnTrials)):
 
-        InterNode1 = np.random.randint(0, TotNodes)
-        InterNode2 = np.random.randint(0, TotNodes)
-        while(int(InterNode1/ClusterNodes) == int(InterNode2/ClusterNodes)):
-            InterNode2 = np.random.randint(0, TotNodes)
-
-        G.add_edge(InterNode1,InterNode2)
-
+        # G = ConnectRand(G, 1, Clusters)
+        G = ConnectNext(G, 1, Clusters)
 
         if j == 0:
             G_init = G.copy()
@@ -99,79 +118,93 @@ def ClusterArrivalProb(G,ConnTrials,steps,InterClusterConn,gamma,eta):
     return G_init, G, probs_init, probs, ArrivalProbs, TotInterConn
 
 
-Clusters = 2 # initial number of clusters
-ClustersLegend = Clusters # for plotting, value of 'Clusters' changes
-ClusterNodes = 20 # number of nodes in each cluster
-ClusterConnections = 40 # number of edges in each cluster
-InterClusterConn = 5 # initial number of edges between clusters
-gamma = 1.0
-eta = 1.0
-steps = 4*Clusters # steps taken by quantum walker
-ConnTrials = 100 # each trial adds an inter-cluster edge
-clusterTrials = 6 # each trial adds a cluster
+
+if __name__ == '__main__':
+
+    ClusterNodes = 20 # number of nodes in each cluster
+    ClusterConnections = 40 # number of edges in each cluster
+    InterClusterConn = 5 # initial number of edges between clusters
+    gamma = 1.0
+    eta = 1.0
+    ConnTrials = 10 # each trial adds an inter-cluster edge
+    ClusterTrials = 3 # each trial adds a cluster
+    TotTrials = 1
 
 
-ArrivalProbsAll = []
-MyGraphsAll = []
-MyProbsAll = []
-MyPosAll = []
-MyTotNodesAll = []
+    ArrivalProbsAll = []
+    MyGraphsAll = []
+    MyProbsAll = []
+    MyPosAll = []
+    MyTotNodesAll = []
+    ArrivalProbsAvg = []
+
+    for t in range(TotTrials):
+        Clusters = 2 # initial number of clusters
+        ClustersLegend = Clusters # for plotting, value of 'Clusters' changes
+        steps = 4*Clusters # steps taken by quantum walker
+
+        print('trial: ', t)
+
+        for c in range(ClusterTrials):
+            '''calculate arrival probability wrt number of connections between clusters, for graphs with increasing number of clusters'''
+
+            print(Clusters, ' clusters, ', 'total number of nodes: ', Clusters*ClusterNodes)
+
+            MyGraph = ClusterGraph(Clusters,ClusterNodes,ClusterConnections) # generate graph (no inter cluster connections)
+            G = MyGraph[0]
+            pos = MyGraph[1]
+            TotNodes = MyGraph[2]
+
+            MyClusterProb = ClusterArrivalProb(G,ConnTrials,steps,Clusters,InterClusterConn,gamma,eta) # add inter cluster connections and measure arrival prob
+
+            G_init = MyClusterProb[0]
+            G = MyClusterProb[1]
+            probs_init = MyClusterProb[2]
+            probs = MyClusterProb[3]
+            ArrivalProbs = MyClusterProb[4]
+            TotInterConn = MyClusterProb[5]
+
+            MyGraphsAll.append((G_init,G))
+            MyProbsAll.append((probs_init, probs))
+            ArrivalProbsAll.append(ArrivalProbs)
+            MyPosAll.append(pos)
+            MyTotNodesAll.append(TotNodes)
+
+            print('Arrival Prob: ', ArrivalProbs[ConnTrials-1])
+
+            Clusters += 1
 
 
-for c in range(clusterTrials):
-    '''calculate arrival probability wrt number of connections between clusters, for graphs with increasing number of clusters'''
+            ArrivalProbsAvg.append(ArrivalProbs)
+        
+    ArrivalProbsAvg = np.reshape(ArrivalProbsAvg, (TotTrials,len(ArrivalProbs)*ClusterTrials))
+    ArrivalProbsAvg = np.average(ArrivalProbsAvg, axis=0)
 
-    print(Clusters, ' clusters, ', 'total number of nodes: ', Clusters*ClusterNodes)
+    # plot
 
-    MyGraph = ClusterGraph(Clusters,ClusterNodes,ClusterConnections) # generate graph (no inter cluster connections)
-    G = MyGraph[0]
-    pos = MyGraph[1]
-    TotNodes = MyGraph[2]
+    fig = plt.figure()
 
-    MyClusterProb = ClusterArrivalProb(G,ConnTrials,steps,InterClusterConn,gamma,eta) # add inter cluster connections and measure arrival prob
+    ax1 = fig.add_subplot(311)
 
-    G_init = MyClusterProb[0]
-    G = MyClusterProb[1]
-    probs_init = MyClusterProb[2]
-    probs = MyClusterProb[3]
-    ArrivalProbs = MyClusterProb[4]
-    TotInterConn = MyClusterProb[5]
+    node_size_init = MyProbsAll[ClusterTrials-1][0]*(100000/(max(MyProbsAll[ClusterTrials-1][0])*MyTotNodesAll[ClusterTrials-1])) # rescale so size of node is never too small or too big
+    PltNodes = nx.draw_networkx_nodes(MyGraphsAll[ClusterTrials-1][0],MyPosAll[ClusterTrials-1], node_color=MyProbsAll[ClusterTrials-1][0], node_size=node_size_init)
+    pltEdges = nx.draw_networkx_edges(MyGraphsAll[ClusterTrials-1][0],MyPosAll[ClusterTrials-1])
+    col1 = fig.colorbar(PltNodes, label='Probability', shrink=0.9)
 
-    MyGraphsAll.append((G_init,G))
-    MyProbsAll.append((probs_init, probs))
-    ArrivalProbsAll.append(ArrivalProbs)
-    MyPosAll.append(pos)
-    MyTotNodesAll.append(TotNodes)
+    ax2 = fig.add_subplot(312)
 
-    print('Arrival Prob: ', ArrivalProbs[ConnTrials-1])
+    node_size = MyProbsAll[ClusterTrials-1][1]*(100000/(max(MyProbsAll[ClusterTrials-1][1])*MyTotNodesAll[ClusterTrials-1])) # rescale so size of node is never too small or too big
+    PltNodes = nx.draw_networkx_nodes(MyGraphsAll[ClusterTrials-1][1],MyPosAll[ClusterTrials-1], node_color=MyProbsAll[ClusterTrials-1][1], node_size=node_size)
+    PltEdges = nx.draw_networkx_edges(MyGraphsAll[ClusterTrials-1][1],MyPosAll[ClusterTrials-1])
+    col2 = fig.colorbar(PltNodes, label='Probability', shrink=0.9)
 
-    Clusters += 1
+    ax3 = fig.add_subplot(313)
 
+    for p in range(ClusterTrials):
+        plt.plot(TotInterConn,ArrivalProbsAvg[p*ConnTrials:ConnTrials*(p+1)], label=(str(ClustersLegend+p)+' clusters'))
+    plt.ylabel('Arrival Probability')
+    plt.xlabel('Number of inter-cluster edges')
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1.0))
+    plt.subplots_adjust(right=0.8)
 
-# plot
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(311)
-
-node_size_init = MyProbsAll[clusterTrials-1][0]*(100000/(max(MyProbsAll[clusterTrials-1][0])*MyTotNodesAll[clusterTrials-1])) # rescale so size of node is never too small or too big
-PltNodes = nx.draw_networkx_nodes(MyGraphsAll[clusterTrials-1][0],MyPosAll[clusterTrials-1], node_color=MyProbsAll[clusterTrials-1][0], node_size=node_size_init)
-pltEdges = nx.draw_networkx_edges(MyGraphsAll[clusterTrials-1][0],MyPosAll[clusterTrials-1])
-col1 = fig.colorbar(PltNodes, label='Probability', shrink=0.9)
-
-ax2 = fig.add_subplot(312)
-
-node_size = MyProbsAll[clusterTrials-1][1]*(100000/(max(MyProbsAll[clusterTrials-1][1])*MyTotNodesAll[clusterTrials-1])) # rescale so size of node is never too small or too big
-PltNodes = nx.draw_networkx_nodes(MyGraphsAll[clusterTrials-1][1],MyPosAll[clusterTrials-1], node_color=MyProbsAll[clusterTrials-1][1], node_size=node_size)
-PltEdges = nx.draw_networkx_edges(MyGraphsAll[clusterTrials-1][1],MyPosAll[clusterTrials-1])
-col2 = fig.colorbar(PltNodes, label='Probability', shrink=0.9)
-
-ax3 = fig.add_subplot(313)
-
-for p in range(clusterTrials):
-    plt.plot(TotInterConn,ArrivalProbsAll[p], label=(str(ClustersLegend+p)+' clusters'))
-plt.ylabel('Arrival Probability')
-plt.xlabel('Number of inter-cluster edges')
-plt.legend()
-
-plt.show()
+    plt.show()
